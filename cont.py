cc753519445921354145f226359b30eb6d5b4703
fahmirevo <fahmi.velosa1997@gmail.com>
2018-06-16 10:11:01 +0700

    change load

--- cont.py
+++ cont.py
@@ -9,7 +9,7 @@ train_size = 22500
 batch_size = 128
 steps_per_epoch = round(train_size / batch_size)
 
-net = torch.load('net.pt')
+net = torch.load('overnet.pt')
 
 criterion = nn.BCELoss()
 optimizer = optim.Adam(net.parameters())
